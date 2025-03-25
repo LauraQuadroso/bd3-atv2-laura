2a2d8521dc361979e9ab4d33352eec5f96066af6
Laura Quadros <163086963+LauraQuadroso@users.noreply.github.com>
2025-03-25 20:00:45 -0300

    Add files via upload

---
+++ insert-atv2.mongodb.js
@@ -0,0 +1,567 @@
+
+
+// Select the database to use.
+use('BD3-AULA');
+
+
+
+  
+
+db['bd3-nosql-atv2'].insertMany([
+
+
+    {
+      "cod_aluno": 1,
+      "cod_turma": 101,
+      "nome": "João Silva",
+      "cpf": "12345678901",
+      "rg": "123456789",
+      "telefone_aluno": "11987654321",
+      "telefone_responsavel": "11912345678",
+      "email": "joao.silva@gmail.com",
+      "data_nascimento": "2005-03-15 00:00:00"
+    },
+    {
+      "cod_aluno": 2,
+      "cod_turma": 102,
+      "nome": "Maria Oliveira",
+      "cpf": "98765432109",
+      "rg": "987654321",
+      "telefone_aluno": "21911223344",
+      "telefone_responsavel": "21998765432",
+      "email": "maria.oliveira@hotmail.com",
+      "data_nascimento": "2006-07-20 00:00:00"
+    },
+    {
+      "cod_aluno": 3,
+      "cod_turma": 101,
+      "nome": "Pedro Santos",
+      "cpf": "11223344556",
+      "rg": "112233445",
+      "telefone_aluno": "31955667788",
+      "telefone_responsavel": "31944332211",
+      "email": "pedro.santos@yahoo.com",
+      "data_nascimento": "2004-11-10 00:00:00"
+    },
+    {
+      "cod_aluno": 4,
+      "cod_turma": 103,
+      "nome": "Ana Costa",
+      "cpf": "65432198765",
+      "rg": "654321987",
+      "telefone_aluno": "41922334455",
+      "telefone_responsavel": "41988776655",
+      "email": "ana.costa@gmail.com",
+      "data_nascimento": "2007-01-25 00:00:00"
+    },
+    {
+      "cod_aluno": 5,
+      "cod_turma": 102,
+      "nome": "Lucas Pereira",
+      "cpf": "55667788990",
+      "rg": "556677889",
+      "telefone_aluno": "51977889900",
+      "telefone_responsavel": "51966554433",
+      "email": "lucas.pereira@outlook.com",
+      "data_nascimento": "2003-05-02 00:00:00"
+    },
+    {
+      "cod_aluno": 6,
+      "cod_turma": 101,
+      "nome": "Julia Almeida",
+      "cpf": "09876543210",
+      "rg": "098765432",
+      "telefone_aluno": "62999001122",
+      "telefone_responsavel": "62988112233",
+      "email": "julia.almeida@yahoo.com.br",
+      "data_nascimento": "2008-09-18 00:00:00"
+    },
+    {
+      "cod_aluno": 7,
+      "cod_turma": 103,
+      "nome": "Carlos Fernandes",
+      "cpf": "10203040506",
+      "rg": "102030405",
+      "telefone_aluno": "71911223344",
+      "telefone_responsavel": "71955443322",
+      "email": "carlos.fernandes@gmail.com",
+      "data_nascimento": "2002-04-08 00:00:00"
+    },
+    {
+      "cod_aluno": 8,
+      "cod_turma": 102,
+      "nome": "Mariana Souza",
+      "cpf": "60504030201",
+      "rg": "605040302",
+      "telefone_aluno": "83988776655",
+      "telefone_responsavel": "83977665544",
+      "email": "mariana.souza@hotmail.com",
+      "data_nascimento": "2009-02-14 00:00:00"
+    },
+    {
+      "cod_aluno": 9,
+      "cod_turma": 101,
+      "nome": "Rafael Rocha",
+      "cpf": "12110987654",
+      "rg": "121109876",
+      "telefone_aluno": "92966554433",
+      "telefone_responsavel": "92955443322",
+      "email": "rafael.rocha@yahoo.com",
+      "data_nascimento": "2001-10-28 00:00:00"
+    },
+    {
+      "cod_aluno": 10,
+      "cod_turma": 103,
+      "nome": "Camila Gomes",
+      "cpf": "45678901234",
+      "rg": "456789012",
+      "telefone_aluno": "13933221100",
+      "telefone_responsavel": "13922110099",
+      "email": "camila.gomes@gmail.com",
+      "data_nascimento": "2006-06-05 00:00:00"
+    },
+    {
+      "cod_aluno": 11,
+      "cod_turma": 102,
+      "nome": "Felipe Dias",
+      "cpf": "32109876543",
+      "rg": "321098765",
+      "telefone_aluno": "14988776655",
+      "telefone_responsavel": "14977665544",
+      "email": "felipe.dias@outlook.com",
+      "data_nascimento": "2000-08-12 00:00:00"
+    },
+    {
+      "cod_aluno": 12,
+      "cod_turma": 101,
+      "nome": "Isabela Lima",
+      "cpf": "78901234567",
+      "rg": "789012345",
+      "telefone_aluno": "15999001122",
+      "telefone_responsavel": "15988112233",
+      "email": "isabela.lima@yahoo.com.br",
+      "data_nascimento": "2007-12-01 00:00:00"
+    },
+    {
+      "cod_aluno": 13,
+      "cod_turma": 103,
+      "nome": "Vinicius Ribeiro",
+      "cpf": "23456789012",
+      "rg": "234567890",
+      "telefone_aluno": "16911223344",
+      "telefone_responsavel": "16955443322",
+      "email": "vinicius.ribeiro@gmail.com",
+      "data_nascimento": "2003-03-22 00:00:00"
+    },
+    {
+      "cod_aluno": 14,
+      "cod_turma": 102,
+      "nome": "Laura Carvalho",
+      "cpf": "87654321098",
+      "rg": "876543210",
+      "telefone_aluno": "17988776655",
+      "telefone_responsavel": "17977665544",
+      "email": "laura.carvalho@hotmail.com",
+      "data_nascimento": "2009-07-09 00:00:00"
+    },
+    {
+      "cod_aluno": 15,
+      "cod_turma": 101,
+      "nome": "Gustavo Pereira",
+      "cpf": "90123456789",
+      "rg": "901234567",
+      "telefone_aluno": "18966554433",
+      "telefone_responsavel": "18955443322",
+      "email": "gustavo.pereira@yahoo.com",
+      "data_nascimento": "2001-05-17 00:00:00"
+    },
+    {
+      "cod_aluno": 16,
+      "cod_turma": 103,
+      "nome": "Beatriz Martins",
+      "cpf": "34567890123",
+      "rg": "345678901",
+      "telefone_aluno": "19933221100",
+      "telefone_responsavel": "19922110099",
+      "email": "beatriz.martins@gmail.com",
+      "data_nascimento": "2006-11-29 00:00:00"
+    },
+    {
+      "cod_aluno": 17,
+      "cod_turma": 102,
+      "nome": "Eduardo Mendes",
+      "cpf": "21098765432",
+      "rg": "210987654",
+      "telefone_aluno": "21988776655",
+      "telefone_responsavel": "21977665544",
+      "email": "eduardo.mendes@outlook.com",
+      "data_nascimento": "2000-01-19 00:00:00"
+    },
+    {
+      "cod_aluno": 18,
+      "cod_turma": 101,
+      "nome": "Fernanda Rocha",
+      "cpf": "76543210987",
+      "rg": "765432109",
+      "telefone_aluno": "22999001122",
+      "telefone_responsavel": "22988112233",
+      "email": "fernanda.rocha@yahoo.com.br",
+      "data_nascimento": "2008-04-03 00:00:00"
+    },
+    {
+      "cod_aluno": 19,
+      "cod_turma": 103,
+      "nome": "Ricardo Silva",
+      "cpf": "10111213141",
+      "rg": "101112131",
+      "telefone_aluno": "23911223344",
+      "telefone_responsavel": "23955443322",
+      "email": "ricardo.silva@gmail.com",
+      "data_nascimento": "2003-09-14 00:00:00"
+    },
+    {
+      "cod_aluno": 20,
+      "cod_turma": 102,
+      "nome": "Juliana Oliveira",
+      "cpf": "41424344454",
+      "rg": "414243444",
+      "telefone_aluno": "24988776655",
+      "telefone_responsavel": "24977665544",
+      "email": "juliana.oliveira@hotmail.com",
+      "data_nascimento": "2005-02-28 00:00:00"
+    },
+    {
+      "cod_aluno": 21,
+      "cod_turma": 101,
+      "nome": "Bruno Costa",
+      "cpf": "56575859505",
+      "rg": "565758595",
+      "telefone_aluno": "25966554433",
+      "telefone_responsavel": "25955443322",
+      "email": "bruno.costa@yahoo.com",
+      "data_nascimento": "2007-06-11 00:00:00"
+    },
+    {
+      "cod_aluno": 22,
+      "cod_turma": 103,
+      "nome": "Amanda Santos",
+      "cpf": "91011121314",
+      "rg": "910111213",
+      "telefone_aluno": "26933221100",
+      "telefone_responsavel": "26922110099",
+      "email": "amanda.santos@gmail.com",
+      "data_nascimento": "2002-12-07 00:00:00"
+    },
+    {
+      "cod_aluno": 23,
+      "cod_turma": 102,
+      "nome": "Daniela Pereira",
+      "cpf": "31415161718",
+      "rg": "314151617",
+      "telefone_aluno": "27988776655",
+      "telefone_responsavel": "27977665544",
+      "email": "daniela.pereira@outlook.com",
+      "data_nascimento": "2009-08-21 00:00:00"
+    },
+    {
+      "cod_aluno": 24,
+      "cod_turma": 101,
+      "nome": "Matheus Almeida",
+      "cpf": "81920212223",
+      "rg": "819202122",
+      "telefone_aluno": "28999001122",
+      "telefone_responsavel": "28988112233",
+      "email": "matheus.almeida@yahoo.com.br",
+      "data_nascimento": "2001-04-16 00:00:00"
+    },
+    {
+      "cod_aluno": 25,
+      "cod_turma": 103,
+      "nome": "Carolina Fernandes",
+      "cpf": "23242526272",
+      "rg": "232425262",
+      "telefone_aluno": "29911223344",
+      "telefone_responsavel": "29955443322",
+      "email": "carolina.fernandes@gmail.com",
+      "data_nascimento": "2006-10-03 00:00:00"
+    },
+    {
+        "cod_aluno": 26,
+        "cod_turma": 102,
+        "nome": "Rodrigo Souza",
+        "cpf": "72829303132",
+        "rg": "728293031",
+        "telefone_aluno": "30988776655",
+        "telefone_responsavel": "30977665544",
+        "email": "rodrigo.souza@hotmail.com",
+        "data_nascimento": "2008-03-07 00:00:00"
+      },
+      {
+        "cod_aluno": 27,
+        "cod_turma": 101,
+        "nome": "Gabriela Rocha",
+        "cpf": "13343536373",
+        "rg": "133435363",
+        "telefone_aluno": "31966554433",
+        "telefone_responsavel": "31955443322",
+        "email": "gabriela.rocha@yahoo.com",
+        "data_nascimento": "2000-09-22 00:00:00"
+      },
+      {
+        "cod_aluno": 28,
+        "cod_turma": 103,
+        "nome": "Leonardo Gomes",
+        "cpf": "48495051525",
+        "rg": "484950515",
+        "telefone_aluno": "32933221100",
+        "telefone_responsavel": "32922110099",
+        "email": "leonardo.gomes@gmail.com",
+        "data_nascimento": "2004-05-08 00:00:00"
+      },
+      {
+        "cod_aluno": 29,
+        "cod_turma": 102,
+        "nome": "Larissa Dias",
+        "cpf": "63646566676",
+        "rg": "636465666",
+        "telefone_aluno": "33988776655",
+        "telefone_responsavel": "33977665544",
+        "email": "larissa.dias@outlook.com",
+        "data_nascimento": "2007-11-15 00:00:00"
+      },
+      {
+        "cod_aluno": 30,
+        "cod_turma": 101,
+        "nome": "Thiago Lima",
+        "cpf": "98990001020",
+        "rg": "989900010",
+        "telefone_aluno": "34999001122",
+        "telefone_responsavel": "34988112233",
+        "email": "thiago.lima@yahoo.com.br",
+        "data_nascimento": "2003-01-05 00:00:00"
+      },
+      {
+        "cod_aluno": 31,
+        "cod_turma": 103,
+        "nome": "Letícia Ribeiro",
+        "cpf": "20212223242",
+        "rg": "202122232",
+        "telefone_aluno": "35911223344",
+        "telefone_responsavel": "35955443322",
+        "email": "leticia.ribeiro@gmail.com",
+        "data_nascimento": "2009-06-28 00:00:00"
+      },
+      {
+        "cod_aluno": 32,
+        "cod_turma": 102,
+        "nome": "Fábio Carvalho",
+        "cpf": "52535455565",
+        "rg": "525354555",
+        "telefone_aluno": "36988776655",
+        "telefone_responsavel": "36977665544",
+        "email": "fabio.carvalho@hotmail.com",
+        "data_nascimento": "2001-02-18 00:00:00"
+      },
+      {
+        "cod_aluno": 33,
+        "cod_turma": 101,
+        "nome": "Natália Pereira",
+        "cpf": "87888990919",
+        "rg": "878889909",
+        "telefone_aluno": "37966554433",
+        "telefone_responsavel": "37955443322",
+        "email": "natalia.pereira@yahoo.com",
+        "data_nascimento": "2005-08-04 00:00:00"
+      },
+      {
+        "cod_aluno": 34,
+        "cod_turma": 103,
+        "nome": "Vinicius Martins",
+        "cpf": "12131415161",
+        "rg": "121314151",
+        "telefone_aluno": "38933221100",
+        "telefone_responsavel": "38922110099",
+        "email": "vinicius.martins@gmail.com",
+        "data_nascimento": "2007-12-12 00:00:00"
+      },
+      {
+        "cod_aluno": 35,
+        "cod_turma": 102,
+        "nome": "Isadora Mendes",
+        "cpf": "47484950515",
+        "rg": "474849505",
+        "telefone_aluno": "39988776655",
+        "telefone_responsavel": "39977665544",
+        "email": "isadora.mendes@outlook.com",
+        "data_nascimento": "2002-04-29 00:00:00"
+      },
+      {
+        "cod_aluno": 36,
+        "cod_turma": 101,
+        "nome": "Guilherme Rocha",
+        "cpf": "72737475767",
+        "rg": "727374757",
+        "telefone_aluno": "40999001122",
+        "telefone_responsavel": "40988112233",
+        "email": "guilherme.rocha@yahoo.com.br",
+        "data_nascimento": "2009-03-11 00:00:00"
+      },
+      {
+        "cod_aluno": 37,
+        "cod_turma": 103,
+        "nome": "Manuela Silva",
+        "cpf": "18192021222",
+        "rg": "181920212",
+        "telefone_aluno": "41911223344",
+        "telefone_responsavel": "41955443322",
+        "email": "manuela.silva@gmail.com",
+        "data_nascimento": "2001-07-25 00:00:00"
+      },
+      {
+        "cod_aluno": 38,
+        "cod_turma": 102,
+        "nome": "Ricardo Oliveira",
+        "cpf": "53545556575",
+        "rg": "535455565",
+        "telefone_aluno": "42988776655",
+        "telefone_responsavel": "42977665544",
+        "email": "ricardo.oliveira@hotmail.com",
+        "data_nascimento": "2006-01-09 00:00:00"
+      },
+      {
+        "cod_aluno": 39,
+        "cod_turma": 101,
+        "nome": "Lívia Costa",
+        "cpf": "88899001020",
+        "rg": "888990010",
+        "telefone_aluno": "43966554433",
+        "telefone_responsavel": "43955443322",
+        "email": "livia.costa@yahoo.com",
+        "data_nascimento": "2003-05-15 00:00:00"
+      },
+      {
+        "cod_aluno": 40,
+        "cod_turma": 103,
+        "nome": "Henrique Santos",
+        "cpf": "23242526272",
+        "rg": "232425262",
+        "telefone_aluno": "44933221100",
+        "telefone_responsavel": "44922110099",
+        "email": "henrique.santos@gmail.com",
+        "data_nascimento": "2008-09-01 00:00:00"
+      },
+      {
+        "cod_aluno": 41,
+        "cod_turma": 102,
+        "nome": "Valentina Pereira",
+        "cpf": "58596061626",
+        "rg": "585960616",
+        "telefone_aluno": "45988776655",
+        "telefone_responsavel": "45977665544",
+        "email": "valentina.pereira@outlook.com",
+        "data_nascimento": "2000-02-29 00:00:00"
+      },
+      {
+        "cod_aluno": 42,
+        "cod_turma": 101,
+        "nome": "Samuel Almeida",
+        "cpf": "93949596979",
+        "rg": "939495969",
+        "telefone_aluno": "46999001122",
+        "telefone_responsavel": "46988112233",
+        "email": "samuel.almeida@yahoo.com.br",
+        "data_nascimento": "2007-08-17 00:00:00"
+      },
+      {
+        "cod_aluno": 43,
+        "cod_turma": 103,
+        "nome": "Helena Fernandes",
+        "cpf": "34353637383",
+        "rg": "343536373",
+        "telefone_aluno": "47911223344",
+        "telefone_responsavel": "47955443322",
+        "email": "helena.fernandes@gmail.com",
+        "data_nascimento": "2002-03-05 00:00:00"
+      },
+      {
+        "cod_aluno": 44,
+        "cod_turma": 102,
+        "nome": "Enzo Souza",
+        "cpf": "69707172737",
+        "rg": "697071727",
+        "telefone_aluno": "48988776655",
+        "telefone_responsavel": "48977665544",
+        "email": "enzo.souza@hotmail.com",
+        "data_nascimento": "2009-04-21 00:00:00"
+      },
+      {
+        "cod_aluno": 45,
+        "cod_turma": 101,
+        "nome": "Alice Rocha",
+        "cpf": "04050607080",
+        "rg": "040506070",
+        "telefone_aluno": "49966554433",
+        "telefone_responsavel": "49955443322",
+        "email": "alice.rocha@yahoo.com",
+        "data_nascimento": "2001-10-07 00:00:00"
+      },
+      {
+        "cod_aluno": 46,
+        "cod_turma": 103,
+        "nome": "Theo Gomes",
+        "cpf": "39404142434",
+        "rg": "394041424",
+        "telefone_aluno": "50933221100",
+        "telefone_responsavel": "50922110099",
+        "email": "theo.gomes@gmail.com",
+        "data_nascimento": "2006-06-19 00:00:00"
+      },
+      {
+        "cod_aluno": 47,
+        "cod_turma": 102,
+        "nome": "Sophia Dias",
+        "cpf": "74757677787",
+        "rg": "747576777",
+        "telefone_aluno": "51988776655",
+        "telefone_responsavel": "51977665544",
+        "email": "sophia.dias@outlook.com",
+        "data_nascimento": "2000-12-03 00:00:00"
+      },
+      {
+        "cod_aluno": 48,
+        "cod_turma": 101,
+        "nome": "Miguel Lima",
+        "cpf": "09101112131",
+        "rg": "091011121",
+        "telefone_aluno": "52999001122",
+        "telefone_responsavel": "52988112233",
+        "email": "miguel.lima@yahoo.com.br",
+        "data_nascimento": "2008-05-27 00:00:00"
+      },
+      {
+        "cod_aluno": 49,
+        "cod_turma": 103,
+        "nome": "Heloísa Ribeiro",
+        "cpf": "44454647484",
+        "rg": "444546474",
+        "telefone_aluno": "53911223344",
+        "telefone_responsavel": "53955443322",
+        "email": "heloisa.ribeiro@gmail.com",
+        "data_nascimento": "2003-11-12 00:00:00"
+      },
+      {
+        "cod_aluno": 50,
+        "cod_turma": 102,
+        "nome": "Davi Carvalho",
+        "cpf": "79808182838",
+        "rg": "798081828",
+        "telefone_aluno": "54988776655",
+        "telefone_responsavel": "54977665544",
+        "email": "davi.carvalho@hotmail.com",
+        "data_nascimento": "2005-07-01 00:00:00"
+      }
+    ]);
+    
+    
+
+
